5b0ef1b72122d100a1723063dd4db1f9d1e6dc6f
MiangYou <3135846119@qq.com>
2024-10-20 20:50:40 +0800

    订正图片

--- src/setting.ts
+++ src/setting.ts
@@ -1,6 +1,8 @@
-//用于项目的logo、标题等配置
+// 用于项目的logo、标题等配置
+import logoImage from '@/assets/images/logo.png';
+
 export default {
-  title: "项目测试体验平台",//项目标题
-  logo: "src/assets/images/logo.png",
-  logoHidden:true,//logo组件是否隐藏设置
-}
+  title: "项目测试体验平台", // 项目标题
+  logo: logoImage, // 使用动态导入的图片路径
+  logoHidden: true, // logo组件是否隐藏设置
+};
